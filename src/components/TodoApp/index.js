1119c8997565af7f831755d8e606b3343cb7d9fc
iB Admin <ib_admin@ib005.local>
2019-08-02 14:23:51 +0530

    green state for checking inputbox on keydown enter calls add todo in todostore

--- src/components/TodoApp/index.js
+++ src/components/TodoApp/index.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 import EnterTodo from "./EnterTodo";
 import todoStore from "../../Stores/instances";
-class TodoApp extends Component {}
+class TodoApp extends Component {
+  onPressEnterKey = description => {
+    todoStore.addTodo(description);
+  };
+  render() {
+    return (
+      <>
+        <EnterTodo onPressEnterKey={this.onPressEnterKey} />
+      </>
+    );
+  }
+}
 export default TodoApp;
