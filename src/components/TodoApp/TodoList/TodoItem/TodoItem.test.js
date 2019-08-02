8ceac416731d28dfeb9b4b0095092ddd74907345
iB Admin <ib_admin@ib005.local>
2019-08-02 15:47:56 +0530

    red state for deleting item in todo store on click delete button

--- src/components/TodoApp/TodoList/TodoItem/TodoItem.test.js
+++ src/components/TodoApp/TodoList/TodoItem/TodoItem.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import TodoItem from "./index";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import TodoModel from "../../../../Stores/TodoStore/Model/TodoModel";
+import TodoStore from "../../../../Stores/TodoStore";
 afterEach(cleanup);
 describe("TodoItem", () => {
   it("should able to toggle the task Status of the todo", () => {
@@ -13,5 +14,15 @@ describe("TodoItem", () => {
     fireEvent.click(checkBox);
     expect(todoModel.toggleTaskStatus).toHaveBeenCalledTimes(1);
     expect(todoModel.taskStatus).toBeTruthy();
+  });
+  it("should able to delete todo in todolist on click of delete button", () => {
+    const todoStore = new TodoStore();
+    todoStore.addTodo("jagadish");
+    jest.spyOn(todoStore, "deleteTodo");
+    const { getByTestId } = render(<TodoItem todo={todoStore.todos[0]} />);
+    const deletebutton = getByTestId("delete");
+    expect(todoStore.todos.length).toBe(1);
+    fireEvent.click(deletebutton);
+    expect(todoStore.todos.length).toBe(0);
   });
 });
