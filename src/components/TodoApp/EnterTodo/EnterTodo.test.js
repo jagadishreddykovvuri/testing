561e7f67c127b601fd72e80beed9da9b280db088
iB Admin <ib_admin@ib005.local>
2019-08-02 12:05:49 +0530

    green state for checking inputbox existance

--- src/components/TodoApp/EnterTodo/EnterTodo.test.js
+++ src/components/TodoApp/EnterTodo/EnterTodo.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import EnterTodo from "./index";
 describe("Enter Todo", () => {
   it("should contain input box", () => {
     const { getByPlaceholderText } = render(<EnterTodo />);
---
+++ src/components/TodoApp/EnterTodo/index.js
@@ -0,0 +1,16 @@
+import React, {
+  Component
+} from "react";
+class EnterTodo extends Component {
+  render() {
+    return ( <
+      >
+      <
+      input type = "text"
+      placeholder = "What needs to be Done?" / >
+      <
+      />
+    );
+  }
+}
+export default EnterTodo;
