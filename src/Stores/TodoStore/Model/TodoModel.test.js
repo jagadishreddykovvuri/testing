78931d6e0de61b55c9d43bcfed58be5cc2327121
iB Admin <ib_admin@ib005.local>
2019-08-02 10:46:42 +0530

    red  state for update todo decription

--- src/Stores/TodoStore/Model/TodoModel.test.js
+++ src/Stores/TodoStore/Model/TodoModel.test.js
@@ -9,11 +9,14 @@ describe("TodoStore ", () => {
         todoStore.addTodo("Learn TDD3");
         todoStore.addTodo("Learn TDD4");
     });
-    it("should update todo description", () => {
+    it("should toggle todo status", () => {
         todoStore.todos[2].toggleTaskStatus()
         expect(todoStore.todos[2].taskStatus).toBeTruthy()
         todoStore.todos[2].toggleTaskStatus()
         expect(todoStore.todos[2].taskStatus).toBeFalsy()
-
+    })
+    it("should update todo description", () => {
+        todoStore.todos[3].updateTodoDescription("learn mobx")
+        expect(todoStore.todos[3]).toBe("learn mobx")
     })
 })
