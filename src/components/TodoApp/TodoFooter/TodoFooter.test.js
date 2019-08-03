7baefc9b76ab9d5eee71f37b1709403d0f105650
iB Admin <ib_admin@ib005.local>
2019-08-03 10:29:12 +0530

    refactoring state call change filter function with completed value in todostore on click completed button

--- src/components/TodoApp/TodoFooter/TodoFooter.test.js
+++ src/components/TodoApp/TodoFooter/TodoFooter.test.js
@@ -5,18 +5,20 @@ import TodoFooter from "./index";
 import { filterValues } from "../../../constant";
 afterEach(cleanup);
 describe("TodoFooter", () => {
-  it("should call change filter function with all value in todostore on click all button ", () => {
-    const todoStore = new TodoStore();
+  let todoStore;
+  beforeEach(() => {
+    todoStore = new TodoStore();
     jest.spyOn(todoStore, "changeFilter");
+  });
+  it("should call change filter function with all value in todostore on click all button ", () => {
     const { getByTestId } = render(<TodoFooter todoStore={todoStore} />);
     const allButton = getByTestId("all");
     fireEvent.click(allButton);
     expect(todoStore.changeFilter).toBeCalledWith(filterValues.all);
   });
   it("should call change filter function with completed value in todostore on click completed button ", () => {
-    const todoStore = new TodoStore();
-    jest.spyOn(todoStore, "changeFilter");
     const { getByTestId } = render(<TodoFooter todoStore={todoStore} />);
+
     const completedButton = getByTestId("completed");
     fireEvent.click(completedButton);
     expect(todoStore.changeFilter).toBeCalledWith(filterValues.completed);
