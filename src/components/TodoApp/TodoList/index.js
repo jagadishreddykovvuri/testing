b6cc6a735a6cbb67eea3c2a6f599b3d19d5da4fd
iB Admin <ib_admin@ib005.local>
2019-08-03 09:48:07 +0530

    green state forrender an todo item components upto todolist size

--- src/components/TodoApp/TodoList/index.js
+++ src/components/TodoApp/TodoList/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem/index";
 class TodoList extends Component {
   render() {
-    return <></>;
+    let id = 0;
+    return (
+      <>
+        {this.props.todoStore.todos.map(todo => {
+          id = id + 1;
+          return (
+            <TodoItem key={id} todo={todo} todoStore={this.props.todoStore} />
+          );
+        })}
+      </>
+    );
   }
 }
 export default TodoList;
