c55ff7f997f5f3bb7745876ae52f0b7f729aa9ba
iB Admin <ib_admin@ib005.local>
2019-08-03 10:20:46 +0530

    red state call change filter function with completed value in todostore on click completed button

--- src/components/TodoApp/TodoFooter/TodoFooter.test.js
+++ src/components/TodoApp/TodoFooter/TodoFooter.test.js
@@ -12,4 +12,12 @@ describe("TodoFooter", () => {
     fireEvent.click(allButton);
     expect(todoStore.changeFilter).toBeCalledWith(filterValues.all);
   });
+  it("should call change filter function with completed value in todostore on click completed button ", () => {
+    const todoStore = new TodoStore();
+    jest.spyOn(todoStore, "changeFilter");
+    const { getByTestId } = render(<TodoFooter todoStore={todoStore} />);
+    const completedButton = getByTestId("completed");
+    fireEvent.click(completedButton);
+    expect(todoStore.changeFilter).toBeCalledWith(filterValues.completed);
+  });
 });
