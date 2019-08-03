3a66edc3854e74667ff43a56d99b4199670a57a0
iB Admin <ib_admin@ib005.local>
2019-08-03 10:10:18 +0530

    red state should call change filter function with all value in todostore on click all button

---
+++ src/components/TodoApp/TodoFooter/TodoFooter.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoStore from "../../../Stores/TodoStore/index";
+import { render, fireEvent } from "@testing-library/react";
+import TodoFooter from "./index";
+import filterValue from "../../../constant";
+describe("TodoFooter", () => {
+  it("should call change filter function with all value in todostore on click all button ", () => {
+    const todoStore = new TodoStore();
+    jest.spyOn(todoStore, "changeFilter");
+    const { getByTestId } = render(<TodoFooter todoStore={todoStore} />);
+    const allButton = getByTestId("all");
+    fireEvent.click(allButton);
+    expect(todoStore.changeFilter).toBeCalledWith(filterValue.all);
+  });
+});
